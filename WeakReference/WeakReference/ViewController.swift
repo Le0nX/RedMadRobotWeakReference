7e527b40a25e852224eae7ce07018bb31169a6f6
Le0nX <cloudmindone@gmail.com>
2020-02-14 09:52:09 +0300

    init: project init

---
+++ WeakReference/WeakReference/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeakReference
+//
+//  Created by Denis Nefedov on 14.02.2020.
+//  Copyright © 2020 Den4ik's Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
